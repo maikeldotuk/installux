710d0e9452056266dc6e0f83af5de63820033048
maikeldotuk <maikel@maikel.uk>
2019-02-10 05:26:55 +0000

    First commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require("dotenv").config()
-const app = require("express")()
+const express = require('express')
+const app = express()
 const bodyParser = require("body-parser")
 const { PASS } = process.env
 
 app.use(bodyParser.json())
+
 app.get("/", (req, res) => {
   res.send(`
     ls -ltr
@@ -14,18 +16,18 @@ app.get("/download", (req, res) => {
   res.send(`
     pass=$(zenity --entry Password --text "Enter Password" --title "Security Check")
     curl -d '{"pass":"'$pass'"}' -H 'Content-Type: application/json' http://localhost:3000/secure 2> /dev/null | bash
+
   `)
 })
 
-app.post("/secure", (req, res) => {
+app.post("/secure", (req, res, next) => {
   const { pass } = req.body
   if (pass === PASS) {
-    res.send(`
-   echo 'Inside SSH keys'
-`)
+    res.send(`echo 'Hello'`)
   } else {
     res.send(`echo 'Unauthorised'`)
   }
-})
+} 
+)
 
 app.listen(3000)
